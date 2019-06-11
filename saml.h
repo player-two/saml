3bc5b0c78229873b4b78e63ad9b7ebcf9e1886bc
megalord <jordan.griege@rackspace.com>
2019-06-11 12:03:53 -0500

    Forgot header file

---
+++ saml.h
@@ -0,0 +1,40 @@
+#include <libxml/xmlstring.h>
+#include <libxml/tree.h>
+
+#include <xmlsec/xmlsec.h>
+#include <xmlsec/transforms.h>
+
+
+const xmlChar* XMLNS_ASSERTION;
+const xmlChar* XMLNS_PROTOCOL;
+
+typedef struct {
+  int debug;
+  char* rock_dir;
+} saml_init_opts_t;
+
+typedef struct {
+  xmlChar* id_attr;
+  xmlChar* insert_after_ns;
+  xmlChar* insert_after_el;
+} saml_doc_opts_t;
+
+typedef struct {
+  xmlChar* name;
+  xmlChar** values;
+  int num_values;
+} saml_attr_t;
+
+int saml_init(saml_init_opts_t*);
+void saml_shutdown();
+
+int saml_doc_validate(xmlDoc* doc);
+xmlChar* saml_doc_issuer(xmlDoc* doc);
+xmlChar* saml_doc_session_index(xmlDoc* doc);
+int saml_doc_attrs(xmlDoc* doc, saml_attr_t** attrs, size_t* attrs_len);
+void saml_free_attrs(saml_attr_t* attrs, size_t attrs_len);
+
+xmlSecTransformCtx* saml_sign_binary(xmlSecKey* key, xmlSecTransformId transform_id, unsigned char* data, size_t data_len);
+int saml_verify_binary(xmlSecKey* cert, xmlSecTransformId transform_id, unsigned char* data, size_t data_len, unsigned char* sig, size_t sig_len);
+int saml_sign_doc(xmlSecKey* key, xmlSecTransformId transform_id, xmlDoc* doc, saml_doc_opts_t* opts);
+int saml_verify_doc(xmlSecKeysMngr* mngr, xmlDoc* doc, saml_doc_opts_t* opts);
