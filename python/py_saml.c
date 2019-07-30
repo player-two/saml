1492cb7bffc9d2db62bbd07e8fec8eaf0eb378f2
megalord <jordan.griege@rackspace.com>
2019-07-30 15:30:04 -0500

    Finish up the doc methods

--- python/py_saml.c
+++ python/py_saml.c
@@ -76,6 +76,160 @@ static PyObject* doc_read_file(PyObject* self, PyObject* args) {
 }
 
 
+static PyObject* doc_serialize(PyObject* self, PyObject* args) {
+  PyObject* capsule;
+  if (!PyArg_ParseTuple(args, "O", &capsule)) {
+    return NULL;
+  }
+
+  xmlDoc* doc = (xmlDoc*)PyCapsule_GetPointer(capsule, CAPSULE_XML_DOC);
+  if (doc == NULL) {
+    PyErr_SetString(SamlError, "invalid document value");
+    return NULL;
+  }
+
+  int buf_len;
+  xmlChar* buf;
+  xmlDocDumpMemory(doc, &buf, &buf_len);
+  PyObject* ret = Py_BuildValue("s#", buf, buf_len);
+  xmlFree(buf);
+  return ret;
+}
+
+
+static PyObject* doc_validate(PyObject* self, PyObject* args) {
+  PyObject* capsule;
+  if (!PyArg_ParseTuple(args, "O", &capsule)) {
+    return NULL;
+  }
+
+  xmlDoc* doc = (xmlDoc*)PyCapsule_GetPointer(capsule, CAPSULE_XML_DOC);
+  if (doc == NULL) {
+    PyErr_SetString(SamlError, "invalid document value");
+    return NULL;
+  }
+
+  return PyBool_FromLong((long)saml_doc_validate(doc));
+}
+
+
+static PyObject* doc_root_name(PyObject* self, PyObject* args) {
+  PyObject* capsule;
+  if (!PyArg_ParseTuple(args, "O", &capsule)) {
+    return NULL;
+  }
+
+  xmlDoc* doc = (xmlDoc*)PyCapsule_GetPointer(capsule, CAPSULE_XML_DOC);
+  if (doc == NULL) {
+    PyErr_SetString(SamlError, "invalid document value");
+    return NULL;
+  }
+
+  xmlNode* root = xmlDocGetRootElement(doc);
+  if (root == NULL || root->name == NULL) {
+    Py_RETURN_NONE;
+  }
+  return Py_BuildValue("s", root->name);
+}
+
+
+static PyObject* doc_id(PyObject* self, PyObject* args) {
+  PyObject* capsule;
+  if (!PyArg_ParseTuple(args, "O", &capsule)) {
+    return NULL;
+  }
+
+  xmlDoc* doc = (xmlDoc*)PyCapsule_GetPointer(capsule, CAPSULE_XML_DOC);
+  if (doc == NULL) {
+    PyErr_SetString(SamlError, "invalid document value");
+    return NULL;
+  }
+
+  xmlNode* root = xmlDocGetRootElement(doc);
+  if (root == NULL || root->name == NULL) {
+    Py_RETURN_NONE;
+  }
+
+  xmlChar* id = xmlGetProp(root, (xmlChar*)"ID");
+  if (id == NULL) {
+    Py_RETURN_NONE;
+  }
+
+  PyObject* ret = Py_BuildValue("s", id);
+  xmlFree(id);
+  return ret;
+}
+
+
+static PyObject* doc_name_id(PyObject* self, PyObject* args) {
+  PyObject* capsule;
+  if (!PyArg_ParseTuple(args, "O", &capsule)) {
+    return NULL;
+  }
+
+  xmlDoc* doc = (xmlDoc*)PyCapsule_GetPointer(capsule, CAPSULE_XML_DOC);
+  if (doc == NULL) {
+    PyErr_SetString(SamlError, "invalid document value");
+    return NULL;
+  }
+
+  xmlChar* name_id = saml_doc_name_id(doc);
+  if (name_id == NULL) {
+    Py_RETURN_NONE;
+  }
+
+  PyObject* ret = Py_BuildValue("s", name_id);
+  xmlFree(name_id);
+  return ret;
+}
+
+
+static PyObject* doc_issuer(PyObject* self, PyObject* args) {
+  PyObject* capsule;
+  if (!PyArg_ParseTuple(args, "O", &capsule)) {
+    return NULL;
+  }
+
+  xmlDoc* doc = (xmlDoc*)PyCapsule_GetPointer(capsule, CAPSULE_XML_DOC);
+  if (doc == NULL) {
+    PyErr_SetString(SamlError, "invalid document value");
+    return NULL;
+  }
+
+  xmlChar* issuer = saml_doc_issuer(doc);
+  if (issuer == NULL) {
+    Py_RETURN_NONE;
+  }
+
+  PyObject* ret = Py_BuildValue("s", issuer);
+  xmlFree(issuer);
+  return ret;
+}
+
+
+static PyObject* doc_status_code(PyObject* self, PyObject* args) {
+  PyObject* capsule;
+  if (!PyArg_ParseTuple(args, "O", &capsule)) {
+    return NULL;
+  }
+
+  xmlDoc* doc = (xmlDoc*)PyCapsule_GetPointer(capsule, CAPSULE_XML_DOC);
+  if (doc == NULL) {
+    PyErr_SetString(SamlError, "invalid document value");
+    return NULL;
+  }
+
+  xmlChar* status_code = saml_doc_status_code(doc);
+  if (status_code == NULL) {
+    Py_RETURN_NONE;
+  }
+
+  PyObject* ret = Py_BuildValue("s", status_code);
+  xmlFree(status_code);
+  return ret;
+}
+
+
 static PyObject* doc_session_index(PyObject* self, PyObject* args) {
   PyObject* capsule;
   if (!PyArg_ParseTuple(args, "O", &capsule)) {
@@ -99,14 +253,79 @@ static PyObject* doc_session_index(PyObject* self, PyObject* args) {
 }
 
 
+static PyObject* doc_attrs(PyObject* self, PyObject* args) {
+  PyObject* capsule;
+  if (!PyArg_ParseTuple(args, "O", &capsule)) {
+    return NULL;
+  }
+
+  xmlDoc* doc = (xmlDoc*)PyCapsule_GetPointer(capsule, CAPSULE_XML_DOC);
+  if (doc == NULL) {
+    PyErr_SetString(SamlError, "invalid document value");
+    return NULL;
+  }
+
+  saml_attr_t* attrs;
+  size_t attrs_len;
+  if (saml_doc_attrs(doc, &attrs, &attrs_len) < 0) {
+    Py_RETURN_NONE;
+  }
+
+  PyObject* ret = PyDict_New();
+  PyObject* val;
+  for (int i = 0; i < attrs_len; i++) {
+    if (attrs[i].name != NULL) {
+      val = NULL;
+      switch (attrs[i].num_values) {
+        case 0:
+          val = Py_None;
+          Py_INCREF(val);
+          break;
+        case 1:
+          if (attrs[i].values[0] == NULL) {
+            val = Py_None;
+            Py_INCREF(val);
+          } else {
+            val = Py_BuildValue("s", (char*)attrs[i].values[0]);
+          }
+          break;
+        default: // Create a list of the values
+          val = PyList_New(attrs[i].num_values);
+          for (int j = 0; j < attrs[i].num_values; j++) {
+            if (attrs[i].values[j] == NULL) {
+              PyList_SetItem(val, j, Py_None);
+              Py_INCREF(Py_None);
+            } else {
+              PyList_SetItem(val, j, Py_BuildValue("s", (char*)attrs[i].values[j]));
+            }
+          }
+          break;
+      }
+      PyDict_SetItemString(ret, (char*)attrs[i].name, val);
+    }
+  }
+
+  saml_attrs_free(attrs, attrs_len);
+  return ret;
+}
+
+
 static PyMethodDef saml_funcs[] = {
   {"init", (PyCFunction)init, METH_VARARGS | METH_KEYWORDS, ""},
   {"shutdown", shutdown, METH_VARARGS, ""},
 
   {"doc_read_memory", doc_read_memory, METH_VARARGS, ""},
   {"doc_read_file", doc_read_file, METH_VARARGS, ""},
+  {"doc_serialize", doc_serialize, METH_VARARGS, ""},
+  {"doc_validate", doc_validate, METH_VARARGS, ""},
 
+  {"doc_root_name", doc_root_name, METH_VARARGS, ""},
+  {"doc_id", doc_id, METH_VARARGS, ""},
+  {"doc_issuer", doc_issuer, METH_VARARGS, ""},
+  {"doc_name_id", doc_name_id, METH_VARARGS, ""},
+  {"doc_status_code", doc_status_code, METH_VARARGS, ""},
   {"doc_session_index", doc_session_index, METH_VARARGS, ""},
+  {"doc_attrs", doc_attrs, METH_VARARGS, ""},
 
   {NULL, NULL, 0, NULL}
 };
--- python/t/test_xml.py
+++ python/t/test_xml.py
@@ -4,10 +4,12 @@ import saml
 
 response = None
 
+
 def setUpModule():
     saml.init("/home/jordan/dev/saml/data/")
     global response
     response = saml.doc_read_file("/home/jordan/dev/saml/lua/t/data/response.xml")
+
 
 class TestSessionIndex(unittest.TestCase):
 
@@ -20,8 +22,7 @@ class TestSessionIndex(unittest.TestCase):
     <saml:AuthnStatement AuthnInstant="2014-07-17T01:01:48Z" SessionNotOnOrAfter="2024-07-17T09:01:48Z">
     </saml:AuthnStatement>
   </saml:Assertion>
-</samlp:Response>
-        ''')
+</samlp:Response>''')
 
     def test_returns_none_if_no_attribute(self):
         index = saml.doc_session_index(self.no_index)
@@ -30,3 +31,46 @@ class TestSessionIndex(unittest.TestCase):
     def test_returns_value_if_attribute(self):
         index = saml.doc_session_index(response)
         self.assertEqual(index, "_be9967abd904ddcae3c0eb4189adbe3f71e327cf93")
+
+
+class TestAttrs(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.no_attrs = saml.doc_read_memory('''<samlp:Response xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol" xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion">
+    <saml:AttributeStatement>
+    </saml:AttributeStatement>
+  <saml:Assertion>
+  </saml:Assertion>
+</samlp:Response>''')
+
+    def test_returns_empty_table_when_no_attributes(self):
+        attrs = saml.doc_attrs(self.no_attrs)
+        self.assertDictEqual({}, attrs)
+
+    def test_returns_all_attributes(self):
+        attrs = saml.doc_attrs(response)
+        self.assertDictEqual({
+            'uid': 'test',
+            'mail': 'test@example.com',
+            'eduPersonAffiliation': [ 'users', 'examplerole1' ]
+        }, attrs)
+
+
+class TestIssuer(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.no_issuer = saml.doc_read_memory('''<samlp:Response xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol" xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion">
+  <samlp:Status>
+    <samlp:StatusCode Value="urn:oasis:names:tc:SAML:2.0:status:Success"/>
+  </samlp:Status>
+</samlp:Response>''')
+
+    def test_returns_none_if_no_issuer(self):
+        issuer = saml.doc_issuer(self.no_issuer)
+        self.assertIsNone(issuer)
+
+    def test_returns_value_of_element(self):
+        issuer = saml.doc_issuer(response)
+        self.assertEqual('http://idp.example.com/metadata.php', issuer)
